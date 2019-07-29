df8b9d8769ec086e7f1214719c9f4c599584f6d1
Reginer <282921012@qq.com>
2019-07-29 18:48:59 +0800

    去掉自定义日志拦截器了

--- app/src/main/java/win/regin/mvvm/api/NetworkApi.kt
+++ app/src/main/java/win/regin/mvvm/api/NetworkApi.kt
@@ -1,10 +1,15 @@
 package win.regin.mvvm.api
 
 import com.google.gson.GsonBuilder
+import com.orhanobut.logger.Logger
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import win.regin.mvvm.BuildConfig
 import win.regin.mvvm.model.Urls
+
 
 /**
  * @author :Reginer in  2019/6/18 20:49.
@@ -20,10 +25,28 @@ object NetworkApi {
             )
     }
 
+    /**
+     * 配置http
+     */
     private fun getOkHttpClient(): OkHttpClient {
-        return OkHttpClient.Builder().apply { addInterceptor(LogInterceptor()) }.build()
+        return OkHttpClient.Builder().apply {
+            if (BuildConfig.DEBUG) addInterceptor(getInterceptor())
+        }.build()
     }
 
+    /**
+     * 配置拦截器
+     */
+    private fun getInterceptor(): Interceptor {
+        val rLog = object : HttpLoggingInterceptor.Logger {
+            override fun log(message: String) {
+                Logger.i(message)
+            }
+        }
+        return HttpLoggingInterceptor(rLog).apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
 }
 
 
