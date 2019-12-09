e8d9d8ff27eda552ad2313e793fdffcf01d89dfd
Reginer <282921012@qq.com>
2019-12-09 13:44:03 +0800

    String转换List方法修改

--- buildSrc/src/main/java/MvvmExt.kt
+++ buildSrc/src/main/java/MvvmExt.kt
@@ -35,7 +35,7 @@ object Version {
     const val objectboxVersion = "2.4.1"
     const val loggerVersion = "2.2.0"
     const val libVersionCode = 11
-    const val libVersionName = "1.9.1"
+    const val libVersionName = "1.9.2"
 }
 
 object MvvmExt {
